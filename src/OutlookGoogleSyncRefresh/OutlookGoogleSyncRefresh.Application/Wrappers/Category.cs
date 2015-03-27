0465a1e949f0c49528dcb3bbb8dc9d8c147223ac
Akanksha Gaur <akankshagaur@outlook.com>
2015-03-27 17:54:57 +0530

    Added category

---
+++ src/OutlookGoogleSyncRefresh/OutlookGoogleSyncRefresh.Application/Wrappers/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using Microsoft.Office.Interop.Outlook;
+
+namespace OutlookGoogleSyncRefresh.Application.Wrappers
+{
+    public class Category
+    {
+        public Color Color { get; set; }
+
+        public string HexValue { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public OlCategoryColor OutlookColor { get; set; }
+    }
+}
