c6358708e29fde4cc386dcf9e8d4a350072b0f8c
Ankesh Dave <smartyad@gmail.com>
2015-04-10 11:28:18 +0530

    Bumped Version Number to 1.3.1.1

--- src/OutlookGoogleSyncRefresh/Shared/SharedAssemblyProperties.cs
+++ src/OutlookGoogleSyncRefresh/Shared/SharedAssemblyProperties.cs
@@ -11,5 +11,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.3.2.0")]
-[assembly: AssemblyFileVersion("1.3.2.0")]
+[assembly: AssemblyVersion("1.3.1.1")]
+[assembly: AssemblyFileVersion("1.3.1.1")]
