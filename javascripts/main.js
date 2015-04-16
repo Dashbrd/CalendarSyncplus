43ea7944ba0aac69e335f25e08eb9534a49ef2ee
Akanksha Gaur <akankshagaur@outlook.com>
2015-04-16 18:50:01 +0530

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,14 @@
-console.log('This would be the main JS file.');
+// Add your javascript here
+$(function()
+{
+  function printRepoCount() 
+  {
+    var responseObj = JSON.parse(this.responseText);
+    var imgSrc = "https://img.shields.io/badge/Downloads "+responseObj.tag_name+"-"+responseObj.assets[0].download_count+"-blue.svg" 
+    $('#ResponseTag').attr("src", imgSrc);
+  }
+  var request = new XMLHttpRequest();
+  request.onload = printRepoCount;
+  request.open('get', 'https://api.github.com/repos/ankeshdave/calendarsyncplus/releases/latest', true)
+  request.send()
+});
