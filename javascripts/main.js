f1f00802ac5a388a8c9b930ca73aa1f148655d0e
Akanksha Gaur <akankshagaur@outlook.com>
2015-04-24 08:49:45 +0530

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,14 +1,14 @@
 // Add your javascript here
-$(function()
-{
-  function printRepoCount() 
-  {
-    var responseObj = JSON.parse(this.responseText);
-    var imgSrc = "https://img.shields.io/badge/Downloads-"+responseObj.assets[0].download_count+"-blue.svg" 
-    $('#ResponseTag').attr("src", imgSrc);
-  }
-  var request = new XMLHttpRequest();
-  request.onload = printRepoCount;
-  request.open('get', 'https://api.github.com/repos/ankeshdave/calendarsyncplus/releases/latest', true)
-  request.send()
-});
+//$(function()
+//{
+  //function printRepoCount() 
+  //{
+    //var responseObj = JSON.parse(this.responseText);
+    //var imgSrc = "https://img.shields.io/badge/Downloads-"+responseObj.assets[0].download_count+"-blue.svg" 
+    //$('#ResponseTag').attr("src", imgSrc);
+  //}
+  //var request = new XMLHttpRequest();
+  //request.onload = printRepoCount;
+  //request.open('get', 'https://api.github.com/repos/ankeshdave/calendarsyncplus/releases/latest', true)
+  //request.send()
+//});
