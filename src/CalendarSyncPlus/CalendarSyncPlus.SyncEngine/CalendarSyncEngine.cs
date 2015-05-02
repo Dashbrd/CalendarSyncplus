8a844c5c9df3d0a05ac8bb2fd9c17c9bb9047a49
akankshagaur <akankshagaur@outlook.com>
2015-05-02 12:37:33 +0530

    Release Notes added

--- src/CalendarSyncPlus/CalendarSyncPlus.SyncEngine/CalendarSyncEngine.cs
+++ src/CalendarSyncPlus/CalendarSyncPlus.SyncEngine/CalendarSyncEngine.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Composition;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ using CalendarSyncPlus.SyncEngine.Interfaces;
 
 namespace CalendarSyncPlus.SyncEngine
 {
+    [Export(typeof(ICalendarSyncEngine))]
     public class CalendarSyncEngine : ICalendarSyncEngine
     {
 
