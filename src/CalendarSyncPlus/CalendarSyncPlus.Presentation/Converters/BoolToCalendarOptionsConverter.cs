5678389680ee5bd8804ea70116a2cf65a028a056
Akanksha Gaur <akankshagaur@outlook.com>
2015-07-06 17:31:02 +0530

    Added missing converters

---
+++ src/CalendarSyncPlus/CalendarSyncPlus.Presentation/Converters/BoolToCalendarOptionsConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using CalendarSyncPlus.Domain.Models;
+
+namespace CalendarSyncPlus.Presentation.Converters
+{
+    public class BoolToCalendarOptionsConverter: IValueConverter
+    {
+        private CalendarEntryOptionsEnum target;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            CalendarEntryOptionsEnum mask = (CalendarEntryOptionsEnum)parameter;
+            this.target = (CalendarEntryOptionsEnum)value;
+            return ((mask & this.target) != 0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                if (value == null || parameter == null)
+                {
+                    return false;
+                }
+                var isValid = (bool)value;
+                var parameterServiceType = (CalendarEntryOptionsEnum)Enum.Parse(typeof(CalendarEntryOptionsEnum), parameter.ToString());
+                if (isValid)
+                {
+                    return target | parameterServiceType;
+                }
+                if (parameterServiceType == CalendarEntryOptionsEnum.Attendees)
+                {
+                    target &= ~CalendarEntryOptionsEnum.AttendeesToDescription;
+                }
+                return target &= ~parameterServiceType;
+            }
+            catch (Exception)
+            {
+            }
+            return CalendarEntryOptionsEnum.None;
+        }
+
+    }
+}
---
+++ src/CalendarSyncPlus/CalendarSyncPlus.Presentation/Converters/OutlookOptionsToVisibilityConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using CalendarSyncPlus.Domain.Models;
+
+namespace CalendarSyncPlus.Presentation.Converters
+{
+    public class OutlookOptionsToVisibilityConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (parameter == null)
+                return Visibility.Collapsed;
+            var parameterString = parameter.ToString();
+            
+            var parameterValue = (OutlookOptionsEnum)Enum.Parse(value.GetType(), parameterString);
+
+            return ((OutlookOptionsEnum)value).HasFlag(parameterValue) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                if (value == null || parameter == null)
+                {
+                    return false;
+                }
+                var isValid = (bool) value;
+                var parameterServiceType =
+                    (OutlookOptionsEnum)Enum.Parse(typeof(OutlookOptionsEnum), parameter.ToString());
+                if (isValid)
+                {
+                    return parameterServiceType;
+                }
+            }
+            catch (Exception)
+            {
+            }
+            return OutlookOptionsEnum.None;
+        }
+
+        #endregion
+    }
+}
