e7470a9877f19602aa1cce05807b94c83971d1e7
Ankesh Dave <smartyad@gmail.com>
2017-03-05 15:06:53 +0530

    Added Test project

---
+++ src/CalendarSyncPlus/CalendarSyncPlus.SyncEngineTest/CalendarSyncEngineTest.cs
@@ -0,0 +1,43 @@
+﻿using Xunit;
+using CalendarSyncPlus.SyncEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalendarSyncPlus.SyncEngineTest
+{
+    public class CalendarSyncEngineTest
+    {
+        [Fact()]
+        public void GetSourceEntriesToDeleteTest()
+        {
+
+        }
+
+        [Fact()]
+        public void GetSourceEntriesToAddTest()
+        {
+
+        }
+
+        [Fact()]
+        public void GetDestEntriesToDeleteTest()
+        {
+
+        }
+
+        [Fact()]
+        public void GetDestEntriesToAddTest()
+        {
+
+        }
+
+        [Fact()]
+        public void ClearTest()
+        {
+
+        }
+    }
+}
